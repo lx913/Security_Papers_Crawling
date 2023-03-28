dca30ef0b9c1c2c194e60531090314cdf6ac5529
lee3yearsold <56989190+lee3yearsold@users.noreply.github.com>
2023-03-28 22:08:53 +0800

    Add files via upload

--- SP_Crawling.py
+++ SP_Crawling.py
@@ -7,8 +7,17 @@ import re
 import time
 import argparse
 
-conference_support = ["ccs"]
+conference_support = ["sp"]
 url = {'dblp':'https://dblp.org/db/conf/',}
+headers = {
+            'Accept': 'application/json,text/plain,*/*',
+            'Accept-Encoding': 'gzip,deflate,br',
+            'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Connection': 'keep-alive',
+            'Content-Length': '122',
+            'Content-Type': 'application/json',
+            'Referer': 'https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=mechanical',
+            'User-Agent': 'Mozilla/5.0 '}
 
 def Crawling(conference = None, year = None, save_folder = 'paper/', keywords= None):
     if conference == None:
@@ -33,15 +42,15 @@ def Crawling(conference = None, year = None, save_folder = 'paper/', keywords= N
     soup = BeautifulSoup(html, "html.parser")
 
     # all href
-    papers = soup.find_all("a",href=lambda x: x and re.search(r"doi\.org/10\.1145/\d{7}\.\d{7}", x))
+    papers = soup.find_all("a",href=lambda x: x and re.compile(r'doi\.org/10\.1109/.*(\d{7})').search(x))
 
     # all doi, including keynote
     pdf_links = []
-    pattern = r"doi\.org/(.+)"
+    pattern = r'doi\.org/10\.1109/.*(\d{7})'
 
     for paper in papers:
         pdf_link = paper.get("href")
-        match_res = 'https://dl.acm.org/doi/pdf/' + re.search(pattern, pdf_link).group(1)
+        match_res = 'https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=' + re.search(pattern, pdf_link).group(1)
         if match_res not in pdf_links:
             pdf_links.append(match_res)
 
@@ -49,9 +58,14 @@ def Crawling(conference = None, year = None, save_folder = 'paper/', keywords= N
         print("No Keywords, Download All Papers")
 
     for pdf_link in pdf_links:
-        print(pdf_link)
+        print('Latent Link: ',pdf_link)
         pdf_response = requests.get(pdf_link)
-        pdf_data = pdf_response.content
+        pdf_newurl = pdf_response.content
+        pdf_loc = BeautifulSoup(pdf_newurl, "html.parser")
+        pdf_url = pdf_loc.find_all(src=True)
+        pdf_true_link = pdf_url[-1]['src']
+        print('True Link: ', pdf_true_link)
+        pdf_data = requests.get(pdf_true_link).content
         stream = BytesIO(pdf_data)
         pdf_reader = PyPDF2.PdfReader(stream)
         if len(pdf_reader.pages) <= 6:
@@ -91,7 +105,7 @@ def Crawling(conference = None, year = None, save_folder = 'paper/', keywords= N
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-C', '--conference', default='ccs', help="conference for crawling")
+    parser.add_argument('-C', '--conference', default='sp', help="conference for crawling")
     parser.add_argument('-Y', '--year', default='2022', help="year for crawling")
     parser.add_argument('-F', '--save_folder', default="paper/", help="where paper saved")
     parser.add_argument('-K', '--keywords', default=None, nargs='+', help="Key words you want")
